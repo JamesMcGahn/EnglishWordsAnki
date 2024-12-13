ff8efe17865ad928f1782d0fbcb8e9513d5cf4ba
James McGahn <jmcgahn@gmail.com>
2024-12-13 11:52:47 -0500

    add status to model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .definition_model import DefinitionModel
-from .word_model import WordModel
+from .word_model import Status, WordModel
 from .words_model import WordsModel
 
-__all__ = ["WordModel", "WordsModel", "DefinitionModel"]
+__all__ = ["WordModel", "WordsModel", "DefinitionModel", "Status"]
--- models/word_model.py
+++ models/word_model.py
@@ -1,8 +1,22 @@
 from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class Status(Enum):
+    ADDED = auto()
+    TO_BE_DEFINED = auto()
+    DEFINDED = auto()
+    TO_BE_AUDIO = auto()
+    AUDIO = auto()
+    ANKI_SYNCED = auto()
+    SKIPPED_AUDIO = auto()
+    SKIPPED_DEFINED = auto()
+    SKIPPED_ANKI_SYNCED = auto()
 
 
 @dataclass
 class WordModel:
+    status: Status
     guid: str
     word: str
     definition: str
