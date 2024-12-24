778f33798628f63ad4f8c9b1afe0be2e2ba8ce6b
James McGahn <jmcgahn@gmail.com>
2024-12-24 11:14:55 -0500

    add tray icon and menu, add tray resources

--- views/main_window.py
+++ views/main_window.py
@@ -1,6 +1,13 @@
 from PySide6.QtCore import QSize, QThread, Signal
-from PySide6.QtGui import QCloseEvent, QFont
-from PySide6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtGui import QAction, QCloseEvent, QFont, QFontDatabase, QIcon
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QMenu,
+    QPushButton,
+    QSystemTrayIcon,
+    QVBoxLayout,
+    QWidget,
+)
 
 from services.logger import Logger
 from views.layout import CentralWidget
@@ -14,7 +21,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.app = app
-        self.setWindowTitle("Custom MainWindow")
+        self.setWindowTitle("English Dictionary")
         self.setObjectName("MainWindow")
         self.resize(875, 985)
         self.setMaximumSize(QSize(875, 985))
@@ -37,6 +44,46 @@ class MainWindow(QMainWindow):
         self.centralWidget.send_logs.connect(self.logger.insert)
         self.appshutdown.connect(self.logger.close)
         self.appshutdown.connect(self.centralWidget.notified_app_shutting)
+
+        app_icon = QIcon()
+        app_icon.addFile(":/system_icons/tray_icon_16.png", QSize(16, 16))
+        app_icon.addFile(":/system_icons/tray_icon_24.png", QSize(24, 24))
+        app_icon.addFile(":/system_icons/tray_icon_48.png", QSize(48, 48))
+        app_icon.addFile(":/system_icons/tray_icon_256.png", QSize(256, 256))
+        self.app.setWindowIcon(app_icon)
+
+        # Set system tray icon
+        tray_icon = QSystemTrayIcon(app_icon, self.app)
+        tray_menu = QMenu()
+        maximize_action = QAction("Maximize", self)
+        minimize_action = QAction("Minimize", self)
+        quit_action = QAction("Quit", self)
+        maximize_action.triggered.connect(self.showNormal)
+        minimize_action.triggered.connect(self.showMinimized)
+        quit_action.triggered.connect(self.close)
+        tray_menu.addAction(maximize_action)
+        tray_menu.addAction(minimize_action)
+        tray_menu.addAction(quit_action)
+        tray_icon.setContextMenu(tray_menu)
+        tray_icon.show()
+
+        tray_icon.activated.connect(self.on_tray_icon_click)
+
+    def on_tray_icon_click(self, reason: QSystemTrayIcon.ActivationReason) -> None:
+        """Handle tray icon click events. If minimized, show window as normal and bring to the font.
+        If the window is already displayed, minimize the window.
+        Args:
+            reason (QSystemTrayIcon.ActivationReason): ActivationReason event when user clicks on the tray icon
+        Returns:
+            None: This function does not return a value.
+        """
+        if reason == QSystemTrayIcon.Trigger:
+            # If the window is minimized, show it
+            if self.isMinimized():
+                self.showNormal()
+                self.activateWindow()  # Bring the window to the front
+            else:
+                self.showMinimized()
 
     def closeEvent(self, event: QCloseEvent) -> None:
         """
